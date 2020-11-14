bd0f21efe63ecc43e8979fb44c61216db8bcb714
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-14 21:28:09 +0530

    Welcome to the Employee Wage Computation program using java

---
+++ EmpWage.java
@@ -0,0 +1,6 @@
+public class EmpWage {
+	public static void main(String args[]){
+		System.out.println("                                     ");
+		System.out.println("Welcome to Employee Wage Computation program using java ");
+}
+}
