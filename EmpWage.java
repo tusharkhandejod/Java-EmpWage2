baba79d0f4af27d7eceea72e981351097c76c86e
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-17 15:56:00 +0530

    Code for Daily Employee Wage  is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,29 @@
-class EmployeeAttendance{
-	final static int present=1;
-	public void checkAttendance(){
-		int check=(int)(Math.floor(Math.random()*10)%2);
-		if(present==check)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
-				}
+//Calculate daily wage
+public class UC2EmpDailyWage {
+	public static void main(String[] args) {
+		DailyWage dailywage	= new DailyWage();
+	    dailywage.employeeWage();
+	}
 }
-public class UC1EmpAttendence{
-public static void main(String args[]) {
-	System.out.println("      ");
-	System.out.println("Welcome to the Employee Wage program");
-	EmployeeAttendance attendance=new EmployeeAttendance();
-	attendance.checkAttendance();
+
+
+class DailyWage{
+	final int present = 1;
+    final int empRatePerHour = 20;
+    final int empHours = 8;
+    public void employeeWage(){       //method for calculating emp salary
+    	for(int Day=1;Day<=20;Day++) {
+    	int salary = 0;
+    	int check = (int)(Math.floor(Math.random()*10)%2);
+    	if(present == check){
+    		System.out.println("Employee is present");
+    		salary = empRatePerHour * empHours; //salary for the given hours
+            System.out.println(salary);
+        }
+    else{
+    	    System.out.println("Employee is absent");
+    	    System.out.println(salary);
+            }
+    }
 }
 }
