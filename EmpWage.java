583d0a505b44633623c0faa0ddcdccb499fb537b
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-17 16:31:18 +0530

    Code for saving total wage for each company is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,56 +1,66 @@
-//calculating employee wage for different companies by taking inputs from user
-public class UC8EmpWageDiffCompanies {
-	final static int isabsent = 0; 
-	final static int isFullTime = 1;
-     final static int isPartTime = 2;
-    
-     public static void calculate(String company_name,int empRatePerHour,int totalDays,int totalHours){
-    	 System.out.println("                             ");
-    	 System.out.println("                             ");
-	     System.out.println("Company Name= "+company_name);
-	     System.out.println("Employee's status= ");
-    	 System.out.println("Day   Attendance    DailyWage  TotalWorkingHours TotalMonthlySallery");
-    	 int hours = 0;
-	     int days = 0;
-	     int empHours;
-	     int totalSalary = 0;
-	     String status;
-	     while(hours <=totalHours && days<totalDays){
-	    	     int salary = 0;
-	    	     int check = (int)(Math.floor(Math.random()*10)%3);
-	             switch(check){
-	             case isPartTime:
-	                     empHours = 4;
-	                     status="HalfTime";
-	                     break;
-	             case isFullTime:
-	                     empHours = 8;
-	                     status="FullTime";
-	                     break;
-	             default :
-	                     empHours = 0;
-	                     status="Absent**";
-	             }
-	             salary = empRatePerHour*empHours;
-                 totalSalary = totalSalary+salary;
-                 System.out.println((days+1)+"\t"+status+"\t"+salary+"\t\t"+hours+"\t\t"+totalSalary);
-                 hours += empHours;
-	             days += 1;
-	     }
-	     int totalEmpWage=hours*empRatePerHour;
-	     System.out.println("                             ");
-	     System.out.println("Total Monthly Salary of the employee in the "+company_name+" company is: "+totalEmpWage);
-     }
-     public static void main(String args[]){
-    		System.out.println("                             ");
-    		System.out.println("Welcome to the Employee Wage problem using java");
-    		System.out.println("                             ");
-    		calculate("Amazon",700,20,180);
-       	    calculate("Microsoft",500,24,200);
-       	    calculate("Flipkart",400,26,230);
-       	    calculate("Wipro",300,28,250);
-       	    calculate("Accenture",500,22,200);
-     }
-
+class EmpWageBuilder{
+	    final int absent=1;
+	    final int isFullTime=1;
+        final int isPartTime=2;
+        private int empRatePerHour;
+        private int totalDays;
+        private int totalHours;
+        private String company;
+        private int totalSalary;
+        
+        EmpWageBuilder(String company,int empRatePerHour,int totalDays,int totalHours){
+        	this.company = company;
+	        this.empRatePerHour = empRatePerHour;
+	        this.totalDays = totalDays;
+	        this.totalHours = totalHours;
+        }
+        public void calculate(){
+	        int hours = 0;
+	        int days = 0;
+	        int salary = 0;
+	        int empHours;
+	        while(hours<=totalHours && days<totalDays){
+	              int check = (int)(Math.floor(Math.random()*10)%3);
+	                switch(check){
+	                case isPartTime:
+	                        empHours = 4;
+	                        break;
+	                case isFullTime:
+	                        empHours = 8;
+	                        break;
+	                default :
+	                        empHours = 0;
+	                }
+	                hours += empHours;
+	                days += 1;
+	                salary = empRatePerHour * empHours;
+	                totalSalary = totalSalary + salary;
+	        }
+        }
+    public String toString(){
+	        return "Total Employee wage for " + company +" is " + (totalSalary);
+	}
 }
-
+public class UC9EmpWageEachComp{
+	public static void main(String args[]){
+		    EmpWageBuilder Amazon=new EmpWageBuilder("Amazon",150,20,180);
+			Amazon.calculate();
+			System.out.println(Amazon);
+			
+			EmpWageBuilder Microsoft=new EmpWageBuilder("Microsoft",500,24,200);
+        	Microsoft.calculate();
+        	System.out.println(Microsoft);
+        	
+        	EmpWageBuilder Flipkart=new EmpWageBuilder("Flipkart",200,26,230);
+        	Flipkart.calculate();
+        	System.out.println(Flipkart);
+        	
+        	EmpWageBuilder Wipro=new EmpWageBuilder("Wipro",120,28,250);
+        	Wipro.calculate();
+        	System.out.println(Wipro);
+        	
+        	EmpWageBuilder Accenture=new EmpWageBuilder("Accenture",500,24,200);
+        	Accenture.calculate();
+        	System.out.println(Accenture);
+	}
+}
