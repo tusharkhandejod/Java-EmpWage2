82aa5af4886a03815a4e56be1426bfa92c7887f4
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-17 17:43:47 +0530

    Code for to get theis added

--- EmpWage.java
+++ EmpWage.java
@@ -1,27 +1,28 @@
-//Store the total wage along with daily wage
+//UC14EmpWageBuilder Ability to get total wage when queried by company
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Map;
 
-interface IComputeWageOfEmployee {  
+interface IComputeEmpWageForComp {                //if its public needs to be created in another file
 	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 }
 
-class CompanyEmployeeWage {  
+class CompEmployeeWage {
 	public final String company;
 	public final int empRatePerHour;
 	public final int numOfWorkingDays;
 	public final int maximumHoursPerMonth;
 	public int totalWage;
-	public CompanyEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
+	public CompEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
 		this.company = company;
 		this.empRatePerHour = empRatePerHour;
 		this.numOfWorkingDays = numOfWorkingDays;
 		this.maximumHoursPerMonth = maximumHoursPerMonth;
 	}
-	public void setTotalEmpWage(int totalWage){
+	public void setTotalEMpWage(int totalWage){
 		this.totalWage=totalWage;
 	}
 	public String toString() {
@@ -29,32 +30,37 @@ class CompanyEmployeeWage {
 	}
 }
 
-public class UC13EmpWageBuilder implements IComputeWageOfEmployee{
+public class UC14EmpWageBuilder implements IComputeEmpWageForComp{
 	public static final int absent=1;
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;
-	private ArrayList<CompanyEmployeeWage> empList;
-	private LinkedList<Integer>dailyWage;
-	public UC13EmpWageBuilder() {
-		empList = new ArrayList<>();
-		dailyWage = new LinkedList<>();
+	private ArrayList<CompEmployeeWage> empList;
+	private Map<String,CompEmployeeWage> empWageMap;
+	public UC14EmpWageBuilder() {
+		empList=new ArrayList<>();
+		empWageMap=new HashMap<>();
 	}
 	@Override
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
-		CompanyEmployeeWage empWage=new CompanyEmployeeWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
+		CompEmployeeWage empWage = new CompEmployeeWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
 		empList.add(empWage);
+		empWageMap.put(company,empWage);
 	}
 
 	@Override
 	public void computeEmpWage() {
 		for(int i=0;i<empList.size();i++) {
-			empList.get(i).setTotalEmpWage(this.computeEmpWage(empList.get(i)));
+			empList.get(i).setTotalEMpWage(this.computeEmpWage(empList.get(i)));
 			System.out.println(empList.get(i));
 		}	
 		
 	}
-	HashMap<String,LinkedList> companyDailyWage=new HashMap<>();
-	private  int computeEmpWage(CompanyEmployeeWage companyEmpWage) {
+	@Override
+	public int getTotalWage(String company) {
+		return empWageMap.get(company).totalWage;
+	}
+	
+	private  int computeEmpWage(CompEmployeeWage companyEmpWage) {
 		int empHours=0, totalEmpHours=0, totalWorkingDays=0;
 		 while(totalEmpHours<=companyEmpWage.maximumHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays){
              int check=(int)(Math.floor(Math.random()*10)%3);
@@ -68,26 +74,22 @@ public class UC13EmpWageBuilder implements IComputeWageOfEmployee{
                default :
                        empHours = 0;
                }
-               totalEmpHours += empHours;
-               totalWorkingDays +=1;
-               dailyWage.add(empHours*companyEmpWage.empRatePerHour);
-               
+               totalEmpHours+=empHours;
+               totalWorkingDays+=1;
                }
-		 companyDailyWage.put(companyEmpWage.company, dailyWage);
-		 System.out.println("Daily Wage for the "+companyEmpWage.company+" is :"+dailyWage);
-		 dailyWage.clear();
 		  return (totalEmpHours*companyEmpWage.empRatePerHour);
-		 
+
 	}
-	
 	public static void main(String args[]) {
-		UC13EmpWageBuilder employeeWageBuilder=new UC13EmpWageBuilder();
+		UC14EmpWageBuilder employeeWageBuilder = new UC14EmpWageBuilder();
 		employeeWageBuilder.addCompanyEmpWage("Amazon", 150, 20, 180);
 		employeeWageBuilder.addCompanyEmpWage("Microsoft", 500, 24, 200);
 		employeeWageBuilder.addCompanyEmpWage("Flipkart", 200, 26, 230);
 		employeeWageBuilder.addCompanyEmpWage("Wipro", 120, 28, 250);
 		employeeWageBuilder.addCompanyEmpWage("Accenture", 500, 22, 200);
 		employeeWageBuilder.computeEmpWage();
+
 	}
 	
 }
+
