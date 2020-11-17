074f334717b5eff3e8d4cc6738204f5680006c20
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-17 15:53:52 +0530

    Code for Employee Attendance is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,18 @@
-public class EmpWage {
-	public static void main(String args[]){
-		System.out.println("                                     ");
-		System.out.println("Welcome to Employee Wage Computation program using java ");
+class EmployeeAttendance{
+	final static int present=1;
+	public void checkAttendance(){
+		int check=(int)(Math.floor(Math.random()*10)%2);
+		if(present==check)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+				}
+}
+public class UC1EmpAttendence{
+public static void main(String args[]) {
+	System.out.println("      ");
+	System.out.println("Welcome to the Employee Wage program");
+	EmployeeAttendance attendance=new EmployeeAttendance();
+	attendance.checkAttendance();
 }
 }
