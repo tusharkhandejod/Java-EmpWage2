85be7a342c360f1e2338bae23b1416c5ea377bd3
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-17 16:38:33 +0530

    Code for managing Employee Wage of multiple companies using interface approach is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,55 +1,54 @@
-//Ability to manage emp wage of multiple companies using array
+//UC11EmployeeWageBuilder  Interface approach array
 
-class CompanyEmpWage {
-	
+interface IComputeWage {
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth);
+	public void computeEmpWage();
+}
+
+class CompanyEmpWageCal {
 	public final String company;
 	public final int empRatePerHour;
 	public final int numOfWorkingDays;
 	public final int maximumHoursPerMonth;
 	public int totalWage;
-
-	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
+	public CompanyEmpWageCal(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
 		this.company = company;
 		this.empRatePerHour = empRatePerHour;
 		this.numOfWorkingDays = numOfWorkingDays;
 		this.maximumHoursPerMonth = maximumHoursPerMonth;
 	}
-	
 	public void setTotalEmpWage(int totalWage){
-		this.totalWage = totalWage;
+		this.totalWage=totalWage;
 	}
-	
 	public String toString() {
 		return "Total Employee Wage for "+company+" is "+totalWage;
 	}
-
 }
 
-public class UC10EmpWageBuilder{
+public class UC11EmployeeWageBuilder implements IComputeWage{
 	public static final int absent=1;
 	public static final int isFullTime=1;
 	public static final int isPartTime=2;
-	private int numOfCompany; 
-	private CompanyEmpWage empWageArray[];  
-	
-	public UC10EmpWageBuilder() {  
-		empWageArray=new CompanyEmpWage[5];
+	private int numOfCompany;
+	private CompanyEmpWageCal empWageArray[];
+	public UC11EmployeeWageBuilder() {
+		empWageArray=new CompanyEmpWageCal[5];
 	}
-	
-	private void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth) {
-		empWageArray[numOfCompany]=new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
+	@Override 
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
+		empWageArray[numOfCompany]=new CompanyEmpWageCal(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
 		numOfCompany++;
 	}
-	
-	private void computeEmpWage() {
+
+	@Override
+	public void computeEmpWage() {
 		for(int i=0;i<numOfCompany;i++) {
 			empWageArray[i].setTotalEmpWage(this.computeEmpWage(empWageArray[i]));
 			System.out.println(empWageArray[i]);
-			
-		}
+		}	
+		
 	}
-	
-	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+	private int computeEmpWage(CompanyEmpWageCal companyEmpWage) {
 		int empHours=0, totalEmpHours=0, totalWorkingDays=0;
 		 while(totalEmpHours<=companyEmpWage.maximumHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays){
              int check=(int)(Math.floor(Math.random()*10)%3);
@@ -63,15 +62,14 @@ public class UC10EmpWageBuilder{
                default :
                        empHours = 0;
                }
-               totalEmpHours += empHours;
+               totalEmpHours+=empHours;
                totalWorkingDays += 1;
  }
 		 return totalEmpHours * companyEmpWage.empRatePerHour;
 
 	}
-	
 	public static void main(String args[]) {
-		UC10EmpWageBuilder employeeWageBuilder=new UC10EmpWageBuilder();
+		UC11EmployeeWageBuilder employeeWageBuilder=new UC11EmployeeWageBuilder();
 		employeeWageBuilder.addCompanyEmpWage("Amazon", 150, 20, 180);
 		employeeWageBuilder.addCompanyEmpWage("Microsoft", 500, 24, 200);
 		employeeWageBuilder.addCompanyEmpWage("Flipkart", 200, 26, 230);
